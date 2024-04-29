f5c2b8962473c740c2b120076ce086317ed768a6
Oscar Ossa <oskrlex17@gmail.com>
2024-04-29 10:27:49 -0500

    feat: add fyne

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
 func main() {
 	db, err := NewDatabase()
 	if err != nil {
 		panic(err)
 	}
 
-	mekano := NewMekano(db)
-	mekano.Payment("filepath")
+	a := app.New()
+	w := a.NewWindow("xlsx to mekano")
+
+	button := widget.NewButton("Subir Archivo", func() {
+		dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
+			if err != nil {
+				panic(err)
+			}
+
+			if uc == nil {
+				return
+			}
+
+			defer uc.Close()
+
+			mekano := NewMekano(db)
+			mekano.Payment(uc.URI().Path())
+		}, w)
+	})
+
+	w.SetContent(button)
+	w.ShowAndRun()
+
 }
